9235d8139e302c80f5ea5b6ba294d0d53af8c94d
mojotron <igormalj@gmail.com>
2021-10-31 18:20:33 +0100

    Add different type of tasks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,27 @@
 'use strict';
 import '../styles/reset.css';
 import '../styles/main.css';
+
+const type = document.querySelector('#type');
+const data = document.querySelector('#data');
+
+type.addEventListener('change', function () {
+  const temp = type.options[type.selectedIndex].value;
+  data.innerHTML = '';
+  if (temp === 'text') data.innerHTML = textBoxHtml;
+  else data.innerHTML = listBoxHtml;
+});
+
+const textBoxHtml = `
+  <textarea class="modal__data-wrapper__text" rows="5"></textarea>
+`;
+
+const listBoxHtml = `
+  <input class="modal__input--data" type="text">
+  <ul class="modal__data-wrapper__list">
+    <li class="modal__data-wrapper__list__item">Bread</li>
+    <li class="modal__data-wrapper__list__item">Milk</li>
+    <li class="modal__data-wrapper__list__item">Eggs</li>
+  </ul>
+  <button class="btn btn--add-list-item ">+</button>
+`;
--- src/scripts/task-class.js
+++ src/scripts/task-class.js
@@ -1,3 +1,10 @@
 class Task {
-  constructor() {}
+  constructor(title, deadline, priority, type, data, projectId) {
+    this.title = title;
+    this.deadline = deadline;
+    this.priority = priority;
+    this.type = type;
+    this.data = data;
+    this.projectId = projectId;
+  }
 }
